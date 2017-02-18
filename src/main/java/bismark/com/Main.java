317af13af7923e0f320d41d32b7316c44d8dda6c
Man4ester <byrkovskiy@gmail.com>
2017-02-18 22:47:16 +0200

    init commit

---
+++ src/main/java/bismark/com/Main.java
@@ -0,0 +1,11 @@
+package bismark.com;
+
+/**
+ * Created by Bismark on 18.02.2017.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.print("Hello");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Bismark on 18.02.2017.
+ */
+public class MainTest {
+}
